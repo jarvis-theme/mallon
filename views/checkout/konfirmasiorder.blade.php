28205341186a23d8ac05307ccc0a287dcf7a6370
yogaswara <gistha.yogaswara@gmail.com>
2016-05-21 07:44:50 +0700

    update css konfirmasi pembayaran

--- views/checkout/konfirmasiorder.blade.php
+++ views/checkout/konfirmasiorder.blade.php
@@ -156,37 +156,52 @@
                 @endif 
           
                 @if($order->jenisPembayaran==2)
-                    <h3><center>{{trans('content.step5.confirm_btn')}} Paypal</center></h3><br>
-                    <p>{{trans('content.step5.paypal')}}</p>
-                    {{$paypalbutton}}
+                <div class="well">
+                    <center>
+                        <h2><b>{{trans('content.step5.confirm_btn')}} Paypal</b></h2><hr>
+                        <p>{{trans('content.step5.paypal')}}</p>
+                    </center><br>
+                    <center id="paypal">{{$paypalbutton}}</center>
                     <br>
+                </div>
                 @elseif($order->jenisPembayaran==4) 
                     @if(($checkouttype==1 && $order->status < 2) || ($checkouttype==3 && ($order->status!=6)))
-                    <center>
-                        <h3>{{trans('content.step5.confirm_btn')}} iPaymu</h3><br>
-                        <p>{{trans('content.step5.ipaymu')}}</p>
-                        <a class="btn btn-info" href="{{url('ipaymu/'.$order->id)}}" target="_blank">{{trans('content.step5.ipaymu_btn')}}</a>
-                    </center>
-                    <br>
+                    <div class="well">
+                        <center>
+                            <h2><b>{{trans('content.step5.confirm_btn')}} iPaymu</b></h2><hr>
+                            <p>{{trans('content.step5.ipaymu')}}</p><br>
+                            <a class="btn btn-info" href="{{url('ipaymu/'.$order->id)}}" target="_blank">{{trans('content.step5.ipaymu_btn')}}</a>
+                            <br>
+                        </center>
+                    </div>
                     @endif
                 @elseif($order->jenisPembayaran==5 && $order->status == 0)
+                <div class="well">
                     <center>
-                        <h3><strong>{{trans('content.step5.confirm_btn')}} DOKU MyShortCart</strong></h3><br>
-                        <p>{{trans('content.step5.doku')}}</p>
+                        <h2><b>{{trans('content.step5.confirm_btn')}} DOKU MyShortCart</b></h2><hr>
+                        <p>{{trans('content.step5.doku')}}</p><br>
                         {{ $doku_button }}
+                        <br>
                     </center>
-                    <br>
+                </div>
                 @elseif($order->jenisPembayaran==6 && $order->status == 0)
-                    <h3><center>{{trans('content.step5.confirm_btn')}} Bitcoin</center></h3><br>
-                    <p>{{trans('content.step5.bitcoin')}}</p>
-                    {{$bitcoinbutton}}
-                    <br>
-                @elseif($order->jenisPembayaran == 8 && $order->status == 0)
+                <div class="well">
                     <center>
-                        <h3>{{trans('content.step5.confirm_btn')}} Veritrans</h3><br>
-                        <p>{{trans('content.step5.veritrans')}}</p>
-                        <button class="btn btn-warning" onclick="location.href='{{ $veritrans_payment_url }}'">{{trans('content.step5.veritrans_btn')}}</button>
+                        <h2><b>{{trans('content.step5.confirm_btn')}} Bitcoin</b></h2><hr>
+                        <p>{{trans('content.step5.bitcoin')}}</p><br>
+                        {{$bitcoinbutton}}
+                        <br>
                     </center>
+                </div>
+                @elseif($order->jenisPembayaran == 8 && $order->status == 0)
+                <div class="well">
+                    <center>
+                        <h2><b>{{trans('content.step5.confirm_btn')}} Veritrans</b></h2><hr>
+                        <p>{{trans('content.step5.veritrans')}}</p><br>
+                        <button class="btn-pay" onclick="location.href='{{ $veritrans_payment_url }}'">{{trans('content.step5.veritrans_btn')}}</button>
+                        <br>
+                    </center>
+                </div>
                 @endif
             </div>
         </div>
