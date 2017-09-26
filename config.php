871b2537bb8e782e1ce2f8e16812480d620c2a88
GYO\Jarvis <gistha.yogaswara>
2017-09-26 09:31:44 +0700

    update config layout

--- config.php
+++ config.php
@@ -1,9 +1,7 @@
 <?php
 return array(
 	"banner"=>true,
-	"layout" => array(
-		"index1"=>"Layout dengan sidebar"
-	),
+	"layout" => false,
 	"num_display" => array(
 		"home_product"		=> "8",
 		"main_product"		=> "20",
